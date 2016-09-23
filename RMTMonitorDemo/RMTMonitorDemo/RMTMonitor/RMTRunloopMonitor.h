caaf8c3b5039a3b8b6303f3f20b8897e720b83b4
game3108 <game3108@qq.com>
2016-09-23 17:58:27 +0800

    update rmtmonitor

---
+++ RMTMonitorDemo/RMTMonitorDemo/RMTMonitor/RMTRunloopMonitor.h
@@ -0,0 +1,41 @@
+//
+//  RMTRunloopMonitor.h
+//  RMTMonitorDemo
+//
+//  Created by game3108 on 16/9/23.
+//  Copyright © 2016年 game3108. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol RMTRunloopMonitorDelegate <NSObject>
+
+@optional
+- (void) notifyRunloopMonitor;
+
+@end
+
+
+/**
+ runloop monitor to check ui lag
+ */
+@interface RMTRunloopMonitor : NSObject
+@property (nonatomic, weak) id<RMTRunloopMonitorDelegate> delegate;
+
+/**
+ The check interval.Default is 50.The unit is NSEC_PER_MSEC.
+ */
+@property (nonatomic, assign) NSUInteger checkInterval;
+
+/**
+ The max check time.Default is 5.
+ */
+@property (nonatomic, assign) NSUInteger maxCheckTime;
+
+
++ (instancetype) sharedInstance;
+
+- (void) start;
+- (void) stop;
+
+@end
